715f955eb4cea386984c3926b7adae2980e329f6
jalan <jalanpalmer@gmail.com>
2017-05-04 21:16:24 -0400

    Implement PDF_init

--- pdftotext/pdftotext.cpp
+++ pdftotext/pdftotext.cpp
@@ -1,10 +1,18 @@
 #include <Python.h>
 #include <structmember.h>
 
+#include <poppler/cpp/poppler-document.h>
+#include <poppler/cpp/poppler-global.h>
+#include <poppler/cpp/poppler-page.h>
+
+
+static PyObject* PdftotextError;
+
 typedef struct {
     PyObject_HEAD
     int page_count;
-    // TODO: poppler doc will go here
+    PyObject* data;
+    poppler::document* doc;
 } PDF;
 
 static PyMemberDef PDF_members[] = {
@@ -20,11 +28,44 @@ static PyMemberDef PDF_members[] = {
 
 static int PDF_init(PDF* self, PyObject* args, PyObject* kwds) {
     PyObject* arg;
+    Py_ssize_t len;
+    char* buf;
     static char* kwlist[] = {(char*)"pdf_file", NULL};
 
+    // Clear everything
+    self->page_count = 0;
+    delete self->doc;
+    self->doc = NULL;
+    Py_CLEAR(self->data);
+
+    // Read the data
     if (!PyArg_ParseTupleAndKeywords(args, kwds, "O", kwlist, &arg)) {
+        // Exception already set
         return -1;
     }
+    self->data = PyObject_CallMethod(arg, "read", NULL);
+    if (self->data == NULL) {
+        // Exception already set
+        return -1;
+    }
+
+    // TODO: explicitly check for bytes type for a more helpful error message
+
+    // Create the doc
+    if (PyBytes_AsStringAndSize(self->data, &buf, &len) < 0) {
+        // Exception already set
+        Py_CLEAR(self->data);
+        return -1;
+    }
+    self->doc = poppler::document::load_from_raw_data(buf, len);
+    if (self->doc == NULL) {
+        PyErr_SetString(PdftotextError, "TODO: useful error message");
+        Py_CLEAR(self->data);
+        return -1;
+     }
+
+    // Set the page_count
+    self->page_count = self->doc->pages();
 
     return 0;
 }
@@ -65,44 +106,42 @@ static PyMethodDef PDF_methods[] = {
 
 static PyTypeObject pdftotext_PDFType = {
     PyVarObject_HEAD_INIT(NULL, 0)
-    "pdftotext.PDF",             // tp_name
-    sizeof(PDF),                 // tp_basicsize
-    0,                           // tp_itemsize
-    (destructor)PDF_dealloc,     // tp_dealloc
-    0,                           // tp_print
-    0,                           // tp_getattr
-    0,                           // tp_setattr
-    0,                           // tp_reserved
-    0,                           // tp_repr
-    0,                           // tp_as_number
-    0,                           // tp_as_sequence
-    0,                           // tp_as_mapping
-    0,                           // tp_hash
-    0,                           // tp_call
-    0,                           // tp_str
-    0,                           // tp_getattro
-    0,                           // tp_setattro
-    0,                           // tp_as_buffer
-    Py_TPFLAGS_DEFAULT,          // tp_flags
-    "TODO: docstring for PDF.",  // tp_doc
-    0,                           // tp_traverse
-    0,                           // tp_clear
-    0,                           // tp_richcompare
-    0,                           // tp_weaklistoffset
-    0,                           // tp_iter
-    0,                           // tp_iternext
-    PDF_methods,                 // tp_methods
-    PDF_members,                 // tp_members
-    0,                           // tp_getset
-    0,                           // tp_base
-    0,                           // tp_dict
-    0,                           // tp_descr_get
-    0,                           // tp_descr_set
-    0,                           // tp_dictoffset
-    (initproc)PDF_init,          // tp_init
+    "pdftotext.PDF",                           // tp_name
+    sizeof(PDF),                               // tp_basicsize
+    0,                                         // tp_itemsize
+    (destructor)PDF_dealloc,                   // tp_dealloc
+    0,                                         // tp_print
+    0,                                         // tp_getattr
+    0,                                         // tp_setattr
+    0,                                         // tp_reserved
+    0,                                         // tp_repr
+    0,                                         // tp_as_number
+    0,                                         // tp_as_sequence
+    0,                                         // tp_as_mapping
+    0,                                         // tp_hash
+    0,                                         // tp_call
+    0,                                         // tp_str
+    0,                                         // tp_getattro
+    0,                                         // tp_setattro
+    0,                                         // tp_as_buffer
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,  // tp_flags
+    "TODO: docstring for PDF.",                // tp_doc
+    0,                                         // tp_traverse
+    0,                                         // tp_clear
+    0,                                         // tp_richcompare
+    0,                                         // tp_weaklistoffset
+    0,                                         // tp_iter
+    0,                                         // tp_iternext
+    PDF_methods,                               // tp_methods
+    PDF_members,                               // tp_members
+    0,                                         // tp_getset
+    0,                                         // tp_base
+    0,                                         // tp_dict
+    0,                                         // tp_descr_get
+    0,                                         // tp_descr_set
+    0,                                         // tp_dictoffset
+    (initproc)PDF_init,                        // tp_init
 };
-
-static PyObject* PdftotextError;
 
 static PyModuleDef pdftotextmodule = {
     PyModuleDef_HEAD_INIT,
---
+++ tests/test_pdf.py
@@ -0,0 +1,45 @@
+"""Tests for the PDF class."""
+
+import io
+import unittest
+
+import pkg_resources
+
+import pdftotext
+
+
+class InitTest(unittest.TestCase):
+    """Test using and abusing __init__."""
+
+    def setUp(self):
+        pdf_path = pkg_resources.resource_filename("tests", "test.pdf")
+        with open(pdf_path, "rb") as pdf_file:
+            self.pdf_file = io.BytesIO(pdf_file.read())
+            self.another_pdf_file = io.BytesIO(self.pdf_file.getvalue())
+
+    def test_double_init_success(self):
+        pdf = pdftotext.PDF(self.pdf_file)
+        pdf.__init__(self.another_pdf_file)
+        self.assertEqual(pdf.page_count, 1)
+
+    def test_double_init_failure(self):
+        pdf = pdftotext.PDF(self.pdf_file)
+        with self.assertRaises(AttributeError):
+            pdf.__init__("wrong")
+
+    def test_init_file_in_text_mode(self):
+        text_file = io.StringIO("wrong")
+        with self.assertRaises(TypeError):
+            pdf = pdftotext.PDF(text_file)
+
+    def test_init_invalid_pdf_file(self):
+        pdf_file = io.BytesIO(b"wrong")
+        with self.assertRaises(pdftotext.Error):
+            pdf = pdftotext.PDF(pdf_file)
+
+    def test_no_init(self):
+        class BrokenPDF(pdftotext.PDF):
+            def __init__(self):
+                pass
+        pdf = BrokenPDF()
+        self.assertEqual(pdf.page_count, 0)
