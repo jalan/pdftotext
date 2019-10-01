f231ac7cbb98490c82f93a0be9477d0202b4df19
Jason Woods <jason.woods@datatri.be>
2019-10-01 12:10:08 -0500

    Updated calls to remove os.

--- setup.py
+++ setup.py
@@ -24,13 +24,13 @@ if platform.system() in ["Darwin", "FreeBSD", "OpenBSD"]:
     include_dirs = ["/usr/local/include"]
     library_dirs = ["/usr/local/lib"]
 elif platform.system() in ['Windows']:
-    conda_dir = os.getenv('CONDA_PREFIX')
+    conda_dir = getenv('CONDA_PREFIX')
     if conda_dir is None:
         print("ERROR: CONDA_PREFIX is not found.")
         print("       Install Anaconda or fix missing CONDA_PREFIX and try again.")
         sys.exit(1)
-    anaconda_poppler_include_dir = os.path.join(conda_dir, 'Library\include')
-    anaconda_poppler_library_dir = os.path.join(conda_dir, 'Library\lib')
+    anaconda_poppler_include_dir = path.join(conda_dir, 'Library\include')
+    anaconda_poppler_library_dir = path.join(conda_dir, 'Library\lib')
     include_dirs = [anaconda_poppler_include_dir]
     library_dirs = [anaconda_poppler_library_dir]
 else:
